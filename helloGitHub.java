5a5ede6bf9ae6e59967b49e3bae0f1ab20bfa219
doneamo <jennifertan2009@gmail.com>
2017-04-22 23:11:11 -0700

    Update helloGitHub.java

--- helloGitHub.java
+++ helloGitHub.java
@@ -5,6 +5,7 @@ public class helloGitHub {
 		System.out.println("testing out github with eclipse");
 		System.out.println("now testing a branch");
 		System.out.println("deleted previous change num 1");
+		System.out.println("now editing from online, see if changes will be shown in eclipse");
 	}
 
 }
